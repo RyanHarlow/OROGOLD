cf5676e637af0a46c97c3b07309755f623861d99
bandit <RyanHarlow95@gmail.com>
2020-05-20 16:02:00 -0500

    style tratamiento section

--- index.php
+++ index.php
@@ -37,33 +37,36 @@
         </div>
     </section>
     <section id="tratamientos-section">
-        <h2>NUESTROS TRATAMIENTOS</h2>
-        <p>El tratamiento de la casa de dos horas de duración transporta a los clientes a un oasis de relajación. Incluye un masaje de espalda, hombros, piernas y pies utilizando acietes de masaje</p>
+        <h2 id='tratamientos-title'>NUESTROS TRATAMIENTOS</h2>
+        <div id='tratamientos-content-container'>
+        <p id='tratamientos-content'>El tratamiento de la casa de dos horas de duración transporta a los clientes a un oasis de relajación. Incluye un masaje de espalda, hombros, piernas y pies utilizando acietes de masaje</p>
+        </div>
         <div id='tratamientos-card-container'>
             <div class='tratamiento-card'>
             <div class='img-cropper'>
             <img class='tratamiento-img-tall' src="./img/photo-1516238840914-94dfc0c873ae.jpg" alt="MASAGES CON ORO">
 </div>    
-            <h3>MASAJES CON ORO</h3>
-                <p><span>SPA Orogold ofrece tratamientos especializados de la mano de nuestros profesionales expertos, con productos excepcionales de marcas galardonadas</span></p>
+            <h3 class='card-title'>MASAJES CON ORO</h3>
+                <p class='card-content'><strong>SPA Orogold</strong> ofrece tratamientos especializados de la mano de nuestros profesionales expertos, con productos excepcionales de marcas galardonadas</p>
             </div>
             <div class='tratamiento-card'>
             <div class='img-cropper'>    
             <img class='tratamiento-img' src="./img/photo-1515377905703-c4788e51af15.jpg" alt="EXFOLIACIÓN CON PRODUCTOS NATURALES">
             </div> 
-            <h3>EXFOLIACIÓN CON PRODUCTOS NATURALES</h3>
-                <p>El tratamiento de la casa de dos horas de duración transporta a los clientes a un oasis de relajación. Incluye un masaje de espalda, hombros, piernas y pies</p>
+            <h3 class='card-title'>EXFOLIACIÓN CON PRODUCTOS NATURALES</h3>
+                <p class='card-content'>El tratamiento de la casa de dos horas de duración transporta a los clientes a un oasis de relajación. Incluye un masaje de espalda, hombros, piernas y pies</p>
             </div>
             <div class='tratamiento-card'>
                 <div class='img-cropper'>
             <img class='tratamiento-img' src="./img/photo-1552693673-1bf958298935.jpg" alt="RECONSTRUCCIÓN FACIAL">
             </div>
-            <h3>RECONSTRUCCIÓN FACIAL</h3>
-                <p>El tratamiento de la casa de dos horas de duración transporta a los clientes a un oasis de relajación. Incluye un masaje de espalda, hombros, piernas y pies</p>
+            <h3 class='card-title'>RECONSTRUCCIÓN FACIAL</h3>
+                <p class='card-content'>El tratamiento de la casa de dos horas de duración transporta a los clientes a un oasis de relajación. Incluye un masaje de espalda, hombros, piernas y pies</p>
             </div>
         </div>
     </section>
     
+
     <script src='index.js'></script>
 </body>
 </html>
