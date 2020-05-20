8b4b1abcba205a179b758d2992f5b50d06d8ec17
bandit <RyanHarlow95@gmail.com>
2020-05-19 21:47:54 -0500

    add navbar and google fonts

--- index.php
+++ index.php
@@ -3,11 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="index.css">
     <title>OROGOLD</title>
 </head>
 <body>
-    <nav></nav>
+    <nav id='main-nav'>
+    <div id='nav-container'>
+        <img id='nav-brand' src="./img/cropped-orogold-logo-dgold.png" alt="OROGOLD logo" />
+        <div id='nav-right'>
+        <a id='nav-email-link' href = "mailto: orogoldspa@orogold.com">orogoldspa@orogold.com</a>
+        <button id='nav-reserva-button'>Reserva ahora</button>
+        </div>
+        </div>
+    </nav>
     
     <script src='index.js'></script>
 </body>
