d670c3d2d6ae8a4336dde78163cb810c27f363f6
bandit <RyanHarlow95@gmail.com>
2020-05-20 19:22:27 -0500

    style quienes somos section

--- index.php
+++ index.php
@@ -67,10 +67,12 @@
     </section>
     <section id='quienes-somos-section'>
         <div id='quienes-somos-content-container'>
-            <div id='quienes-somos-text-container'>
-                <h2>QUIENES SOMOS</h2>
-                <p>El lujoso spa en Orogold, en Mexico, ofrece un refugio sublime en pleno corazón de la ciudad.</p>
-                <p>Los huéspedes pueden disfrutar de un gimnasio de última generación, una relajante sauna o duchas a chorro durante su escapada de 5 estrellas en nuestro hotel boutique en París. Por otra parte, nuestra nueva asociación con ZEAL COSMETICS asegura la disponibilidad de una gama de relajantes tratamientos faciales y corporales para mimarse y revitalizarse.</p>
+            <div id='quienes-somos-background-overlay'>
+                <div id='quienes-somos-text-container'>
+                <h2 id='quienes-somos-title'>QUIENES SOMOS</h2>
+                <p class='quienes-somos-text'>El lujoso spa en Orogold, en Mexico, ofrece un refugio sublime en pleno corazón de la ciudad.</p>
+                <p class='quienes-somos-text'>Los huéspedes pueden disfrutar de un gimnasio de última generación, una relajante sauna o duchas a chorro durante su escapada de 5 estrellas en nuestro hotel boutique en París. Por otra parte, nuestra nueva asociación con ZEAL COSMETICS asegura la disponibilidad de una gama de relajantes tratamientos faciales y corporales para mimarse y revitalizarse.</p>
+                </div>
             </div>
         </div>
         <div>
