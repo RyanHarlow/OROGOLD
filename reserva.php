1570be104cf46fde561c8d5abbb690a2d276cb63
bandit <RyanHarlow95@gmail.com>
2020-05-22 06:01:33 -0500

    add additional php comments

--- reserva.php
+++ reserva.php
@@ -1,10 +1,13 @@
 <?php
 
+//input db credentials here
 $servername = "";
 $username = "";
 $password = "";
 $dbname = "orogold";
 
+
+//checks that all variables are included and not empty
 if(!isset($_POST["nombre"]) || !isset($_POST["email"]) || !isset($_POST["telephono"])){
     $data = [ 'error' => 'Por favor incluya todas las entradas' ];
     echo json_encode($data);
@@ -27,16 +30,22 @@ if ($conn->connect_error) {
 }else{
 // echo "Connected successfully";
 
+
+//sql query string
 $sql = "INSERT INTO reserva (nombre, email, telephono) VALUES ('$nombre', '$email', '$telephono')";
+
 
 if ($conn->query($sql) === TRUE) {
     $data = [ 'success' => 'reserva realizada' ];
     echo json_encode($data);
 } else {
+    //checks for duplicate entry
     if(strpos($conn->error, 'Duplicate entry') !== false){
         $data = [ 'error' => 'Ya tienes una cita' ];
         echo json_encode($data);
-    }else{
+    }
+    //handles db error
+    else{
         $data = [ 'error' => 'Error al crear la reserva' ];
         echo json_encode($data);
     }
